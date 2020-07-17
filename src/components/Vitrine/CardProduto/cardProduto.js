f59b71ba65a5b7de737afa5a7b8e323d707d04b7
Bruno Lima <39595498+brunoliimas@users.noreply.github.com>
2020-07-17 08:44:54 -0300

    Update CardProducts.js

--- src/components/Vitrine/CardProduto/cardProduto.js
+++ src/components/Vitrine/CardProduto/cardProduto.js
@@ -1,60 +1,58 @@
 import React from 'react';
 import "./cardProduto.scss";
+import Axios from 'axios';
 
 class CardProduto extends React.Component {
-   state = {
-      produtos: []
-   };
-
-   componentDidMount() {
-      fetch('https://corebiz-test.herokuapp.com/api/v1/products')
-         .then(res => res.json())
-         .then(res => {
-            this.setState({
-               produtos: res,
-            });
-         });
+   constructor(props) {
+      super(props)
+      this.state = {
+         products: []
+      }
    }
 
 
-   //     productId
-   //     productName
-   //     imageUrl
-   //     listPrice
-   //     price
-   //     installments": [
-   //       {
-   //         "quantity": 9,
-   //         "value": 2887
-   //       }
+
+   componentDidMount() {
+      Axios.get('https://corebiz-test.herokuapp.com/api/v1/products')
+         .then(response => {
+            // console.log(response)
+            this.setState({ products: response.data })
+         })
+         .catch(error => {
+            console.log(error)
+         })
+   }
 
 
    render() {
+
+      const { products } = this.state
       return (
          <div id="Produto" className="produto">
-            {this.state.produtos.map(item => (
-               <div key={item.productId} className="card">
-                  <img className="imagem-produto" src={item.imageUrl} alt="" />
-                  <h1 className="nome-produto">{item.productName}</h1>
-                  <span className="estrelas">{item.stars}</span>
-                  <p className="preco-antigo">de R${item.listPrice}</p>
-                  <h2 className="preco-atual">por R$ {item.price}</h2>
-                  <p>ou em 4x de R$ 14,99</p>
-               </div>
-            ))}
-            {/* <ul>
-                    {this.state.produtos.map(item => (
-                        <li key={item.productId}>
-                            <p><b>Nome:</b> {item.productName}</p>
-                            <p><b>Criador:</b> {item.listPrice}</p>
-                            <p><b>Ano de criação:</b> {item.price}</p>
-                            <img src={item.imageUrl} alt=""/>
-                        </li>
-                    ))}
-                </ul> */}
+            {
+               products.length ?
+                  products.map(product =>
+                     <div key={product.productId} className="card">
+                        <img className="imagem-produto" src={product.imageUrl} alt="" />
+
+                        <h1 className="nome-produto">{product.productName}</h1>
+
+                        <span className="estrelas">{product.stars}</span>
+
+                        <p className="preco-antigo">de R${product.listPrice}</p>
+
+                        <h2 className="preco-atual">por R$ {product.price}</h2>
+
+                        <p>ou em 4x de R$ 14,99</p>
+
+                     </div>
+                  ) :
+                  null
+            }
          </div>
       );
    }
 }
 
 export default CardProduto;
+
